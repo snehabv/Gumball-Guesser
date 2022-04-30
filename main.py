6a9a5d2ae471f8695c2bf8ea0935826b89a8b72b
Sneha Singh <102164104+snehabv@users.noreply.github.com>
2022-04-30 00:22:26 +0000

    final project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from turtle import *
+2from turtle import *
 import random
 import turtle as tur
+import time
 tur.Screen().bgcolor("light blue")
 
 secret_number = 0
@@ -16,7 +17,7 @@ number_of_blocks=num_blocks
 gummies=number_of_blocks
 
 '''
-# to debug
+# debug
 print("secret_number: " + str(secret_number))
 print("num_blocks  " + str(num_blocks))
 print("gummies: " + str(gummies))
@@ -24,7 +25,7 @@ print("number_of_blocks: " + str(number_of_blocks))
 '''
 
 colors = ["red", "yellow", "pink", "blue", "purple", "orange"]
-selected_color = random.choice(colors)
+# selected_color = random.choice(colors)
 
 def jar():
   penup()
@@ -39,69 +40,68 @@ def jar():
   
   
 def draw_check():
-    clear()
-    penup()
-    setposition(0,0)
-    pendown()
-    pensize(5)
-    color("green")
-    left(135)
-    forward(50)
-    backward(50)
-    right(90)
-    forward(100)
-    right(45)
+  clear()
+  penup()
+  setposition(0,0)
+  pendown()
+  pensize(5)
+  color("green")
+  left(135)
+  forward(50)
+  backward(50)
+  right(90)
+  forward(100)
+  right(45)
 
 def up_arrow():
-    clear()
-    penup()
-    setposition(0,0)
-    pendown()
-    pensize(5)
-    left(90)
-    forward(100)
-    right(135)
-    forward(50)
-    backward(50)
-    left(270)
-    forward(50)
-    left(135)
+  clear()
+  penup()
+  setposition(0,0)
+  pendown()
+  pensize(5)
+  left(90)
+  forward(100)
+  right(135)
+  forward(50)
+  backward(50)
+  left(270)
+  forward(50)
+  left(135)
     
 def down_arrow():
-    clear()
-    penup()
-    setposition(0,0)
-    pendown()
-    pensize(5)
-    right(90)
-    forward(100)
-    right(135)
-    forward(50)
-    backward(50)
-    left(270)
-    forward(50)
-    right(45)
-   
+  clear()
+  penup()
+  setposition(0,0)
+  pendown()
+  pensize(5)
+  right(90)
+  forward(100)
+  right(135)
+  forward(50)
+  backward(50)
+  left(270)
+  forward(50)
+  right(45)
+ 
 def move_to_row(gummies):
-    x_value = -((gummies*20)/2)
-    y_value = -75+(20*row_value)
-    penup()
-    setposition(x_value,y_value)
-    pendown()
+  x_value = -((gummies*20)/2)
+  y_value = -75+(20*row_value)
+  penup()
+  setposition(x_value,y_value)
+  pendown()
   
 def draw_block_row(gummies):
-    for i in range(gummies):
-      color(random.choice(colors))
-      begin_fill()
-      circle(radius)
-      end_fill()
-      penup()
-      forward(20)
-      pendown()
+  for i in range(gummies):
+    color(random.choice(colors))
+    begin_fill()
+    circle(radius)
+    end_fill()
+    penup()
+    forward(20)
+    pendown()
     
-# debug statement
-# print(num_blocks)
 
+# print(num_blocks)
 
 # Draw the jar at a given speed
 speed(5)
@@ -114,7 +114,7 @@ for i in range(number_of_blocks):
   row_value=row_value+1
   draw_block_row(number_of_blocks)
   number_of_blocks=number_of_blocks-1
-
+start_time = time.time()
 
 if num_blocks == 0:
   secret_number = 0
@@ -134,13 +134,20 @@ elif num_blocks == 7:
   secret_number = 28
 
 '''
-# to debug
+# debug
 print("secret_number: " + str(secret_number))
 print("num_blocks  " + str(num_blocks))
 print("gummies: " + str(gummies))
 print("number_of_blocks: " + str(number_of_blocks))
 '''
 
+def time_convert(sec):
+  mins = sec // 60
+  sec = sec % 60
+  hours = mins // 60
+  mins = mins % 60
+  print("Time = {0}:{1}:{2}".format(int(hours),int(mins),sec))
+  
 
 user_number = int(input("How many Gumballs are in the jar? "))
 if user_number > secret_number:
@@ -149,13 +156,18 @@ elif user_number < secret_number:
   up_arrow()
 else:
   draw_check()
-
+  end_time = time.time()
+  time_lapsed = end_time - start_time
+  time_convert(time_lapsed)
+  
 while user_number != secret_number:
-  user_number = int(input("Keep guessing the number of gumballs "))
+  user_number = int(input("Keep guessing the number of gumballs: "))
   if user_number > secret_number:
     down_arrow()
   elif user_number < secret_number:
     up_arrow()
   else:
     draw_check()
-
+    end_time = time.time()
+    time_lapsed = end_time - start_time
+    time_convert(time_lapsed)
